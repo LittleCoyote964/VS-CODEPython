4fd86daf7c8b32313a7dff4ab375e06377e6f75c
LittleCoyote964 <nickdeleon16@gmail.com>
2025-02-09 22:49:48 -0600

    update files

--- Lab 9/lab9-6.py
+++ Lab 9/lab9-6.py
@@ -2,7 +2,7 @@ num = int(input("Please enter a number:"))
 
 for i in range(1, num + 1):
     for j in range(num - i):
-        print("", end="")
+        print(" ", end="")
     
     for k in range(i):
         print("*", end="")
