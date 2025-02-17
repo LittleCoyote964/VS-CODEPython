591c684f24119390caf826410b797da9e9c8dc30
LittleCoyote964 <nickdeleon16@gmail.com>
2025-02-16 21:30:01 -0600

    added lab 11-3

--- Lab 11/lab11-3.py
+++ Lab 11/lab11-3.py
@@ -1,7 +1,23 @@
-import math
+def prime(n: int) -> bool:
+   
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
 
-k = int(input("Please input k:"))
+    import math
+    upper_limit = int(math.sqrt(n)) + 1
+    for i in range(3, upper_limit, 2):
+        if n % i == 0:
+            return False
+    return True
 
-while k < 0:
-    print("Please input a positive number:")
+user_input = input("Enter an integer: ")
+num = int(user_input) 
 
+if prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
