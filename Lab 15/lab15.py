d88df202b2aba4a59b8927f08bcb37e06d3ed6b9
LittleCoyote964 <nickdeleon16@gmail.com>
2025-02-19 13:43:39 -0600

    Forgot to save one

--- Lab 15/lab15.py
+++ Lab 15/lab15.py
@@ -11,3 +11,16 @@ def display_utility():
     print("2. Email")
     print("3. Note")
     print("4. Main Menu")
+
+def display_game():
+    print("Game menu")
+    print("1. Poker")
+    print("2. Blackjack")
+    print("3. Main Menu")
+
+def display_media():
+    print("Multimedia Menu")
+    print("1. Music Player")
+    print("2. Camera")
+    print("3. Download Youtube")
+    print("4. Main Menu")
