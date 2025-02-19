7bf889c39695e47abfff99a2836e09d846970b36
LittleCoyote964 <nickdeleon16@gmail.com>
2025-02-19 13:05:52 -0600

    Added new files

---
+++ Lab 13/CeltoFar.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+cvalues = [14, -45, 9, 7, 13]
+print(cvalues)
+c = np.array(cvalues)
+print(c)
+print(c * 9 / 5 + 32)
---
+++ Lab 13/lab13-2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+farenValues = [72, 88, 45, 56, 65, 78, 89, 90, 87, 84]
+print(farenValues)
+f = np.array(farenValues)
+print(f)
+print((f -32) * (5/9))
