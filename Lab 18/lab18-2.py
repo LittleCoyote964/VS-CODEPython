db3da072de51a0882ba3e09a5ab5ea06855b852c
LittleCoyote964 <nickdeleon16@gmail.com>
2025-02-26 09:16:01 -0600

    added edits to lab18-2

--- Lab 18/lab18-2.py
+++ Lab 18/lab18-2.py
@@ -4,22 +4,24 @@ class BankAccount:
     
     def deposit(self, amount):
         self.__balance += amount
+        print("Depositing $", amount, "...")
     
     def withdraw(self, amount):
         if self.__balance < amount:
-            print("You do not have enough to withdraw", amount)
+            print("You do not have enough to withdraw $", amount)
         else: 
             self.__balance -= amount
+            print("One moment while we withdraw $", amount,"from your account...")
     
     def get_balance(self):
         return self.__balance
     
 
 B1 = BankAccount(100)
-print("Initial balance:" , B1.get_balance())
+print("Initial balance: $" , B1.get_balance())
 B1.deposit(200)
-print("After deposit:" , B1.get_balance())
+print("After deposit: $" , B1.get_balance())
 B1.withdraw(150)
-print("After first withdrawal:" , B1.get_balance())
+print("After first withdrawal: $" , B1.get_balance())
 B1.withdraw(400)
-print("Final balance:" , B1.get_balance())
+print("Final balance: $" , B1.get_balance())
