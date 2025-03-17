bb73889da574a8082c464da8dd85f43192846433
LittleCoyote964 <nickdeleon16@gmail.com>
2025-03-17 13:13:00 -0500

    Started on the main program, will work on menu now

--- Lab 23/lab23-2.py
+++ Lab 23/lab23-2.py
@@ -10,18 +10,18 @@ def handle_click():
 
 w = tk.Tk()
 w.title("Kilo to Mile")
-w.configure(bg="blue")
+w.configure(bg="maroon")
 w.geometry("600x600")
 fontStyle = tkFont.Font(family="Times New Roman", size=20)
 label1 = tk.Label(text="Please input a value in Kilometer.", font=fontStyle)
 label1.place(x=100, y=100)
-entry1 = tk.Entry(fg="Blue", width=10, font=("Times New Roman", 20))
+entry1 = tk.Entry(fg="maroon", width=10, font=("Times New Roman", 20))
 entry1.place(x=240, y=200)
 button1 = tk.Button(text="Convert!", width=25, height=5, bg="white", fg="Black", command=handle_click)
 button1.place(x=215, y=300)
 button1.bind("<Button-1>", handle_click)
 
-Rlabel = tk.Label(text="", font=fontStyle, bg="Blue", fg="white")
+Rlabel = tk.Label(text="", font=fontStyle, bg="maroon", fg="white")
 Rlabel.place(x=150, y=400)
 
 w.mainloop()
