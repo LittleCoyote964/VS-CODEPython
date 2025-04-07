3a8074b4205cc878e5542566735e3eab6189deab
LittleCoyote964 <nickdeleon16@gmail.com>
2025-04-07 14:13:06 -0500

    Added new labs

---
+++ Lab 24/Lab24-1.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+top = tk.Tk()
+c = tk.Canvas(top, bg='blue', height=872,
+width=1329)
+load = Image.open('cat.jpg')
+img = ImageTk.PhotoImage(load)
+c.create_image(1329/2, 872/2, image=img)
+c.pack()
+top.mainloop()
