cd6c1820f7e80c792e3dc2d73c4b281477f4a098
LittleCoyote964 <nickdeleon@gmail.com>
2025-04-10 14:03:39 -0500

    Added lab 24 - 1

--- Lab 24/Lab24-1.py
+++ Lab 24/Lab24-1.py
@@ -1,9 +1,11 @@
 import tkinter as tk
+import os
 from PIL import Image, ImageTk
 top = tk.Tk()
 c = tk.Canvas(top, bg='blue', height=872,
 width=1329)
-load = Image.open('cat2.jpg')
+print("Current working directory:", os.getcwd())
+load = Image.open("cat.jpg")
 img = ImageTk.PhotoImage(load)
 c.create_image(1329/2, 872/2, image=img)
 c.pack()
