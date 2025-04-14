105ffd7f5434c71c3fb435c4c5602e6ef2dc0bac
LittleCoyote964 <nickdeleon@gmail.com>
2025-04-14 13:29:59 -0500

    Added new lab

--- Lab 24/Lab24-1.py
+++ Lab 24/Lab24-1.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 top = tk.Tk()
 c = tk.Canvas(top, bg='blue', height=872,
 width=1329)
-print("Current working directory:", os.getcwd())
+print("Current working directory:", os.getcwd()) # had to find where to find in what directory was the code trying to look for the image from. 
 load = Image.open("cat.jpg")
 img = ImageTk.PhotoImage(load)
 c.create_image(1329/2, 872/2, image=img)
